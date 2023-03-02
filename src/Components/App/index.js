4f5c735ed4db1c87e4cebda64cc5a1fa89a7c266
tsah012 <tsah012@gmail.com>
2023-03-02 17:04:03 +0200

    some improvments
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import axios from 'axios';
 import AppRouter from '../AppRouter';
 import { useDispatch } from 'react-redux';
@@ -6,29 +6,30 @@ import { LOGGED_IN, LOGGED_OUT, LOGIN_FAILURE } from '../../actions/auth';
 
 function App() {
     const dispatch = useDispatch();
-    async function fetchUserData() {
-        // HERE WE TRY TO FETCH USER DATA WHEN APP IS STARTED.
-        // IF USER IS ALREADY LOGGED IN - DATA WILL RETURN AND WE UPDATE STATE ACCORDINGLY.
-        // OTHERWISE - STATE STARTS AS LOGGED OUT AND WE WILL BE REDIRECTED TO LOGIN PAGE.
-        try {
-            const response = await axios.get('http://localhost:4000/api/user', { withCredentials: true });
+    const fetchUserData = useCallback(
+        async () => {
+            // HERE WE TRY TO FETCH USER DATA WHEN APP IS STARTED.
+            // IF USER IS ALREADY LOGGED IN - DATA WILL RETURN AND WE UPDATE STATE ACCORDINGLY.
+            // OTHERWISE - STATE STARTS AS LOGGED OUT AND WE WILL BE REDIRECTED TO LOGIN PAGE.
+            try {
+                const response = await axios.get('http://localhost:4000/api/user', { withCredentials: true });
 
-            if (response.status === 200) {
-                dispatch({ type: LOGGED_IN, payload: response.data });
+                if (response.status === 200) {
+                    dispatch({ type: LOGGED_IN, payload: response.data });
+                }
+                else {
+                    dispatch({ type: LOGGED_OUT });
+                }
             }
-            else {
-                dispatch({ type: LOGGED_OUT });
+            catch (error) {
+                console.log('error occurred during fetching user data. error:\n' + error);
+                dispatch({ type: LOGIN_FAILURE, payload: error.message || '' });
             }
-        }
-        catch (error) {
-            console.log('error occurred during fetching user data. error:\n' + error);
-            dispatch({ type: LOGIN_FAILURE, payload: error.message || '' });
-        }
-    }
+        }, [dispatch]);
 
     useEffect(() => {
         fetchUserData();
-    }, []);
+    }, [fetchUserData]);
 
     return (
         <div>
